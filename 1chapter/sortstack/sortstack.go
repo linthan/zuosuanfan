cfc52ed809bce7baadf0f4e04f149e953bbaccee
fusikai <fusikai@douyu.tv>
2019-03-25 21:23:19 +0800

    tmp save

--- 1chapter/sortstack/sortstack.go
+++ 1chapter/sortstack/sortstack.go
@@ -31,3 +31,27 @@ func removeMaxValue(s *minstatck.Stack) int {
 		return a
 	}
 }
+
+//SortWrapper1 Wrapper
+func SortWrapper1(s *minstatck.Stack) {
+	help := &minstatck.Stack{}
+	for s.Len() != 0 {
+		ret, _ := s.Pop()
+		for help.Len() != 0 {
+			p, _ := help.Peek()
+			//一定要带上等于 要不然会在上层发生死循环
+			if p <= ret {
+				break
+			}
+			tmp, _ := help.Pop()
+			s.Push(tmp)
+		}
+		help.Push(ret)
+	}
+
+	for help.Len() != 0 {
+		tmp, _ := help.Pop()
+		s.Push(tmp)
+	}
+
+}
--- 1chapter/sortstack/sortstack_test.go
+++ 1chapter/sortstack/sortstack_test.go
@@ -20,3 +20,17 @@ func TestS(t *testing.T) {
 		fmt.Println(s.Pop())
 	}
 }
+
+func TestS1(t *testing.T) {
+	numbers := []int{3, 4, 5, 1, 2, 1}
+	s := &minstatck.Stack{}
+	for _, item := range numbers {
+		s.Push(item)
+
+	}
+	//逆序栈
+	SortWrapper1(s)
+	for range numbers {
+		fmt.Println(s.Pop())
+	}
+}
