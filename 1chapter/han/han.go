e0eb7667cfc86b571df7ec6d7b7ca65f0697fed7
fusikai <fusikai@douyu.tv>
2019-03-25 21:45:57 +0800

    tmp save

--- 1chapter/han/han.go
+++ 1chapter/han/han.go
@@ -1,6 +1,10 @@
 package han
 
-import "fmt"
+import (
+	"github.com/linthan/zuosuanfan/1chapter/minstatck"
+
+	"fmt"
+)
 
 //Han 汉诺塔递归
 func Han(n int, x, y, z string) {
@@ -10,4 +14,14 @@ func Han(n int, x, y, z string) {
 	Han(n-1, x, z, y)
 	fmt.Printf("%s=>%s \n", x, z)
 	Han(n-1, y, x, z)
+}
+
+//Han1 汉诺塔递归
+func Han1(n int, x, y, z *minstatck.Stack) {
+	if n == 0 {
+		return
+	}
+	Han1(n-1, x, z, y)
+	fmt.Println("-------------")
+	Han1(n-1, y, x, z)
 }
